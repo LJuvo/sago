05ade4fd31207e490ed6fb0d7bff3af897fa231a
JuvoS <www.704921698@qq.com>
2019-09-05 00:39:34 +0800

    init at index.htlm

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 let objectProject = {
   site: {
     entry: "src/views/site/main.js", // page 的入口
-    template: "src/public/site.html", // 模板来源
+    template: "src/public/index.html", // 模板来源
     filename: "index.html", // 在 dist/index.html 的输出
     // 当使用 title 选项时，template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
     title: "Index Page",
